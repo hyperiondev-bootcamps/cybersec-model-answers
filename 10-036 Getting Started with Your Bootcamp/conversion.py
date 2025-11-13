e9e2b9a01e13edf32e79d235df27f94df07af82c
Learning Design Team <167773807+HD-LearningDesign@users.noreply.github.com>
2025-11-13 18:06:03 +0200

    Add files via upload

---
+++ 10-036 Getting Started with Your Bootcamp/conversion.py
@@ -0,0 +1,34 @@
+# Pseudocode:
+# 1. Declare the variables num1, num2, num3, and string1
+# 2. Assign the values 99.23, 23, 150, and "100" to the variables num1, num2, num3, and string1 respectively
+# 3. Convert num1 into an integer
+# 4. Convert num2 into a float
+# 5. Convert num3 into a string
+# 6. Convert string1 into an integer
+# 7. Print the variables and their types.
+
+# Python code:
+
+# Declare the variables
+num1 = 99.23
+num2 = 23
+num3 = 150
+string1 = "100"
+
+# Convert num1 into an integer
+num1 = int(num1)
+
+# Convert num2 into an float
+num2 = float(num2)
+
+# Convert num3 into a string
+num3 = str(num3)
+
+# Convert string1 into an integer
+string1 = int(string1)
+
+# Print the variables and their types for verification.
+print(f"{num1} is of {type(num1)}")
+print(f"{num2} is of {type(num2)}")
+print(f"{num3} is of {type(num3)}")
+print(f"{string1} is of {type(string1)}")
